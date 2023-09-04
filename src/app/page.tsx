ac8b66b0af1745f8ab218b0149f7902e7a17a06b
Michael Strupp <michael.strupp@wipotec.com>
2023-09-04 13:13:48 +0200

    Added edit mode
    - Also removed unused svg files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,45 +4,99 @@ import React from "react";
 import Counter from "@/components/counter";
 
 export interface Player {
-  id: number
-  name: string
-  score: number
+  id: number;
+  name: string;
+  score: number;
 }
 
-let lastId = 0;
+const initialPlayers = [
+  { id: 0, name: "Name", score: 0 },
+  { id: 1, name: "Name", score: 0 },
+  { id: 2, name: "Name", score: 0 },
+];
+let nextId = 3;
 
 export default function Home() {
-  const [players, setPlayers] = React.useState<Array<Player>>([]);
+  const [players, setPlayers] = React.useState(initialPlayers);
+  const [edit, setEdit] = React.useState(false);
+  const [goal, setGoal] = React.useState(30);
 
-  function handleChangePlayer(nextPlayer: Player) {
-    console.log(nextPlayer);
+  function handleChangePlayer(player: Player) {
     setPlayers(
-      players.map((player) => {
-        if (player.id == nextPlayer.id) {
-          return nextPlayer;
-        } else {
+      players.map((p) => {
+        if (p.id == player.id) {
           return player;
+        } else {
+          return p;
         }
       })
     );
+
+    if (player.score >= goal) {
+      console.log(player.name + " hat gewonnen!");
+    }
   }
 
   function handleAddPlayer() {
-    setPlayers([...players, { id: lastId, name: "Name", score: 0 }]);
-    console.log(players);
-    lastId += 1;
+    setPlayers([...players, { id: nextId, name: "Name", score: 0 }]);
+    nextId += 1;
+  }
+
+  function handleRemovePlayer(player: Player) {
+    setPlayers(players.filter((p) => p.id != player.id));
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center bg-gray-950 text-gray-200">
       <h1 className="py-10 text-4xl font-semibold">Musikquiz</h1>
-      <div className="flex space-x-10 items-center">
+      <div className="flex space-x-10 items-start">
         {players.map((player) => (
-          <Counter player={player} onChangePlayer={handleChangePlayer} />
+          <Counter
+            player={player}
+            onChangePlayer={handleChangePlayer}
+            onRemovePlayer={handleRemovePlayer}
+            winner={player.score >= goal}
+            edit={edit}
+            key={player.id}
+          />
         ))}
-        <button onClick={handleAddPlayer} className="text-3xl">
-          +
-        </button>
+        <div className="flex flex-col space-y-2 w-6 stroke-gray-200">
+          <button onClick={handleAddPlayer} key={-1} title="Spieler hinzufügen">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+          </button>
+          <button
+            onClick={() => {
+              setEdit(!edit);
+            }}
+            className={
+              "bg-gray-700 stroke-gray-200" + (edit ? " bg-blue-900" : "")
+            }
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
     </main>
   );
--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,44 +1,55 @@
 import React from "react";
-import {Player} from "../app/page"
+import { Player } from "../app/page";
 
 export default function Counter({
   player,
   onChangePlayer,
+  onRemovePlayer,
+  winner,
+  edit,
 }: {
   player: Player;
   onChangePlayer: (player: Player) => void;
+  onRemovePlayer: (player: Player) => void;
+  winner: boolean;
+  edit: boolean;
 }) {
   return (
     <div className="w-28 flex flex-col items-center space-y-6">
-      <input
-        className="w-full bg-gray-900 text-center text-lg"
-        type="text"
-        value={player.name}
-        onChange={(e) => onChangePlayer({ ...player, name: e.target.value })}
-      />
+      {edit ? (
+        <div className="flex space-x-2">
+          <input
+            className="w-full bg-gray-900 text-center text-lg"
+            type="text"
+            value={player.name}
+            onChange={(e) =>
+              onChangePlayer({ ...player, name: e.target.value })
+            }
+          />
+          <button onClick={() => onRemovePlayer(player)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      ) : (
+        <div>{player.name}</div>
+      )}
+
       <button
         onClick={() => onChangePlayer({ ...player, score: player.score + 1 })}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          className="w-full bg-blue-300 stroke-blue-900 rounded-lg px-8 active:scale-95 hover:bg-blue-200"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M4.5 15.75l7.5-7.5 7.5 7.5"
-          />
-        </svg>
-      </button>
-      <p className=" text-4xl font-bold select-none cursor-default">
-        {player.score}
-      </p>
-      <button
-        onClick={() => onChangePlayer({ ...player, score: player.score - 1 })}
+        className="w-full bg-blue-300 stroke-blue-900 rounded-lg px-8 active:scale-95 hover:bg-blue-200"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -46,7 +57,32 @@ export default function Counter({
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-full bg-blue-300 stroke-blue-900 rounded-lg px-8 active:scale-95 hover:bg-blue-200"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.5 15.75l7.5-7.5 7.5 7.5"
+          />
+        </svg>
+      </button>
+      <div
+        className={
+          "text-4xl font-bold select-none cursor-default" +
+          (winner ? " text-yellow-500" : "")
+        }
+      >
+        {player.score}
+      </div>
+      <button
+        onClick={() => onChangePlayer({ ...player, score: player.score - 1 })}
+        className="w-full bg-blue-300 stroke-blue-900 rounded-lg px-8 active:scale-95 hover:bg-blue-200"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
         >
           <path
             strokeLinecap="round"
